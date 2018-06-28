19591182f3d4dba09a7ff9e3456fd54b3f4c33de
Vadim Gorokhov <vadimgiam@gmail.com>
2018-06-29 01:37:30 +0300

    пп7

---
+++ js/gllacy.js
@@ -0,0 +1,14 @@
+<script>
+var link = document.querySelector(".button__feedback");
+
+link.addEventListener("click", function(){
+console.log("Клик по ссылке обратной связи");
+});
+
+
+
+
+
+
+
+</script>
